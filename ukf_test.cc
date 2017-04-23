7643260ef83cc4baf8e4a808195f0719c96e3a15
Mez <mez@jetcode.io>
2017-04-22 23:30:04 -0700

    setting up BDD style test cases for testing

--- ukf_test.cc
+++ ukf_test.cc
@@ -1,8 +1,56 @@
 #include "libs/catch.hpp"
 #include "ukf.h"
 
-
-TEST_CASE( "UKF Generates Sigma Points", "[sigmapoints]") {
+TEST_CASE("UFK TESTS", "[sigmapoints]") {
   Ukf ufk;
-  REQUIRE( 1 == 1 );
+
+  SECTION("GenerateSigmaPoints() Test") {
+    REQUIRE(false);
+  }
+
+  SECTION("PredictSigmaPoints() Test") {
+    REQUIRE(false);
+  }
+
+  SECTION("PredictMeanAndCovariance() Test") {
+    REQUIRE(false);
+  }
+
+  SECTION("PredictMeasurement() Test") {
+    REQUIRE(false);
+  }
+
+  SECTION("Update() Test") {
+    REQUIRE(false);
+  }
+}
+
+SCENARIO("A sensor reading is sent to ProcessMeasurement()", "[use_radar_][use_laser_]") {
+  GIVEN( "A LIDAR measurement" ) {
+    WHEN( "the use_laser_ is set to false" ) {
+      THEN( "the state should not be updated." ) {
+        REQUIRE(false);
+      }
+    }
+
+    WHEN( "the use_laser_ is set to true" ) {
+      THEN( "the state should be updated." ) {
+        REQUIRE(false);
+      }
+    }
+  }
+
+  GIVEN( "A RADAR measurement" ) {
+    WHEN( "the use_radar_ is set to false" ) {
+      THEN( "the state should not be updated." ) {
+        REQUIRE(false);
+      }
+    }
+
+    WHEN( "the use_radar_ is set to true" ) {
+      THEN( "the state should be updated." ) {
+        REQUIRE(false);
+      }
+    }
+  }
 }
